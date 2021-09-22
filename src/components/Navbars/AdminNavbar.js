ad03797ce362549352d5ad802f84ef10bef10ef9
Dinesh Sanala <dnsh4989@gmail.com>
2021-09-22 11:21:39 +0530

    firebase setup

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -198,6 +198,24 @@ function Header() {
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
               >
+                <span className="no-icon">Signup</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">Login</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
                 <span className="no-icon">Log out</span>
               </Nav.Link>
             </Nav.Item>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
@@ -27,13 +16,32 @@ import "./assets/css/demo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import AdminLayout from "layouts/Admin.js";
+import Signup from "components/Signup/Signup";
+
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "PrivateRoute";
+import UpdateProfile from "components/UpdateProfile/UpdateProfile";
+import Dashboard from "components/Dashboard/Dashboard";
+import Login from "components/Login/Login";
+import ForgotPassword from "components/ForgetPassword/ForgetPassword";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
+  <Router>
+    <AuthProvider>
+      <Switch>
+        <PrivateRoute exact path="/" component={Dashboard} />
+        <PrivateRoute path="/update-profile" component={UpdateProfile} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgot-password" component={ForgotPassword} />
+
+        <PrivateRoute
+          path="/admin"
+          render={(props) => <AdminLayout {...props} />}
+        />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </AuthProvider>
+  </Router>,
   document.getElementById("root")
 );
